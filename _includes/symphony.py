74403bd2d3924798ed32c4c2a8edd5cba62c3dbc
1363V4 <105431087+1363V4@users.noreply.github.com>
2022-05-13 12:21:49 +0200

    Update symphony.py

--- _includes/symphony.py
+++ _includes/symphony.py
@@ -1,12 +1,12 @@
 import asyncio
 
 async def foo():
-	pyscript.write("out", "\n", append="True")
+	pyscript.write("out", "‏‏‎ ‎", append="True") #whitechar
 	for _ in range(4):
 		await asyncio.sleep(1)
 		pyscript.write("out", "Tin", append="True")
 	await asyncio.sleep(2)
-	pyscript.write("out", "\n", append="True")
+	pyscript.write("out", "‏‏‎ ‎", append="True")
 	for _ in range(4):
 		await asyncio.sleep(1)
 		pyscript.write("out", "Tin", append="True")
